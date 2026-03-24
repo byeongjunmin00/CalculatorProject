5ca8cbbddab98ab54fa3520ece1587f88d8f37c6
민병준 <minbyeongjun@minbyeongjun-ui-MacBookAir.local>
2026-03-24 19:58:49 +0900

    연산수행역할 변경

--- src/App.java
+++ src/App.java
@@ -13,33 +13,8 @@ public class App {
             System.out.println("사칙연산 기호를 입력하세요: ");
             char op = sc.next().charAt(0);
 
-            int result = 0;
-
-            if (op == '+') {
-                result = num1 + num2;
-            } else if (op == '-') {
-                result = num1 - num2;
-            } else if (op == '*') {
-                result = num1 * num2;
-            } else if (op == '/') {
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                    continue;
-                }
-                result = num1 / num2;
-            } else {
-                System.out.println("잘못된 연산자입니다");
-                continue;
-            }
-            System.out.println("결과: " + result);
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료): ");
-            String answer = sc.next();
-
-            if (answer.equals("exit")) {
-                break;
-            }
+            Calculator cal = new Calculator();
+            int result = cal.calculate(num1, num2, op);
         }
-
-        sc.close();
     }
 }
